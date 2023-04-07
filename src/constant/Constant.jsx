fb47cb829a19dc87ab92d4182bc6be476947e46b
Rishab Singh <rishab.w.singh1234@gmail.com>
2023-04-08 01:38:34 +0530

    jhg

--- src/constant/Constant.jsx
+++ src/constant/Constant.jsx
@@ -1,1 +1,5 @@
-export const API_URL = 'https://192.168.1.8:4000/api';
+// export const API_URL = 'https://192.168.1.8:4000/api';
+// export const WS_URL = 'ws://192.168.1.8:4000';
+
+export const API_URL = 'https://chat-backend-black.vercel.app/api';
+export const WS_URL = 'wss://chat-backend-black.vercel.app';
--- src/providers/SocketProvider.js
+++ src/providers/SocketProvider.js
@@ -1,11 +1,12 @@
 import React, { createContext, useContext, useMemo } from 'react'
+import { WS_URL } from '../constant/Constant';
 
 export const SocketContext = createContext();
 export const useSocket = () => {
     return useContext(SocketContext);
 }
 export const SocketProvider = ({ children }) => {
-    const socket = useMemo(() => new WebSocket("ws://192.168.1.4:4000/ws/video"), [])
+    const socket = useMemo(() => new WebSocket(`${WS_URL}/ws/video`), [])
 
     const sendJSON = (JSONdata) => {
         socket.send(JSON.stringify(JSONdata));
